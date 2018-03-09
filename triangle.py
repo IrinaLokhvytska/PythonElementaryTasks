e1c2d4f0e0e7743658b0ec6bac1238b1c1da1a30
IrinaLokhvytska <irinaramzaeva2@gmail.com>
2018-03-09 19:20:02 +0200

    triangle

--- triangle.py
+++ triangle.py
@@ -3,34 +3,40 @@ import math
 
 def getSquare (name, side1, side2, side3):
     '''Get square of the triangle'''
-    p = 0.5*(int(side1) + int(side2) + int(side3))
-    square = math.sqrt((p*(p-int(side1))*(p-int(side2))*(p-int(side3))))
+    p = 0.5*(side1 + side2 + side3)
+    square = math.sqrt((p*(p-side1)*(p-side2)*(p-side3)))
     output = {name : square}
     return output
 
 def checkThatTriangleExists(side1, side2, side3):
     '''Check side of the triangle for existence'''
     exist = False
-    if (int(side1) + int(side2) >= int(side3)) and (int(side1) + int(side3) >= int(side2)) and (int(side3) + int(side2) >= int(side1)):
+    if (side1 + side2 >= side3) and (side1 + side3 >= side2) and (side3 + side2 >= side1):
         exist = True
     return exist
 
 def validation (name, side1, side2, side3):
     '''Validate values'''
-    if checkEmptyValue (side1) and checkNumbers (side1):
-        if checkEmptyValue (side2) and checkNumbers (side2):
-            if checkEmptyValue (side3) and checkNumbers (side3):
-                if checkEmptyValue (name):
-                    if checkThatTriangleExists(side1, side2, side3):
-                        return getSquare (name, side1, side2, side3)
-                    else:
-                        print('The triangle ' + name + ' does not exist')
+    valid = 0
+    msg = ''
+    triangle = {'name': name, 'side1': side1, 'side2': side2, 'side3': side3}
+    for key, value in triangle.items():
+        if checkEmptyValue(value):
+            if key != 'name':
+                if checkPositiveNumbers(value):
+                    valid += 1
                 else:
-                    print('Name can not be empty')
-            else:
-                print('The side3 of the triangle should be a positive integer')
-        else:  print('The side2 of the triangle should be a positive integer')
-    else:  print('The side1 of the triangle should be a positive integer')
+                    msg += 'The ' + key + ' of the triangle should be a positive integer \n'
+            valid += 1
+        else:
+            msg += 'The ' + key + ' of the triangle can not be empty \n'
+    if valid == 7:
+        if checkThatTriangleExists(int(side1), int(side2), int(side3)):
+            return getSquare (name, int(side1), int(side2), int(side3))
+        else:
+            return 'The triangle ' + name + ' does not exist'
+    else:
+        return msg
 
 def checkEmptyValue (value):
     '''Check that input value isn't empty'''
@@ -39,13 +45,16 @@ def checkEmptyValue (value):
         validation = True
     return validation
 
-def checkNumbers (value):
+def checkPositiveNumbers (value):
      '''Check that input value can be converted to integer'''
+     validation = False
      try:
          int(value)
      except ValueError:
-         return False
-     return True
+         return validation
+     if int(value) > 0:
+         validation = True
+     return validation
 
 name = input('Enter the name of the triangle: ')
 side1 = input('Enter the side1 of the triangle: ')
