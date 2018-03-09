12471c632ae023b404e9ae9e75553a1db24d5085
IrinaLokhvytska <irinaramzaeva2@gmail.com>
2018-03-09 18:53:57 +0200

    chess

--- сhess.py
+++ сhess.py
@@ -4,9 +4,9 @@ def showChess (width, height, char):
     '''Print the chess board '''
     i = 0
     output = ''
-    while i < abs(int(height)):
+    while i < int(height):
         j = 0
-        while j < abs(int(width)):
+        while j < int(width):
             if i % 2 == 0:
                 output += (char + ' ')
             else:
@@ -18,16 +18,23 @@ def showChess (width, height, char):
 
 def validation (width, height, char):
     '''Validate values'''
-    if checkEmptyValue (width) and checkNumbers (width):
-        if checkEmptyValue (height) and checkNumbers (height):
-            if checkEmptyValue (char):
-                return showChess (width, height, char)
-            else:
-                print('Char can not be empty')
+    valid = 0
+    msg = ''
+    chess = {'width': width, 'height': height, 'char': char}
+    for key, value in chess.items():
+        if checkEmptyValue(value):
+            if key != 'char':
+                if checkPositiveNumbers(value):
+                    valid += 1
+                else:
+                    msg += 'The ' + key + ' of the chess should be a positive integer \n'
+            valid += 1
         else:
-            print('The height of the chess should be a positive integer')
-    else:  print('The width of the chess should be a positive integer')
-
+            msg += 'The ' + key + ' of the chess can not be empty \n'
+    if valid == 5:
+        return showChess (width, height, char)
+    else:
+        return msg
 
 def checkEmptyValue (value):
     '''Check that input value isn't empty'''
@@ -36,13 +43,16 @@ def checkEmptyValue (value):
         validation = True
     return validation
 
-def checkNumbers (value):
+def checkPositiveNumbers (value):
      '''Check that input value can be converted to integer'''
+     validation = False
      try:
          int(value)
      except ValueError:
-         return False
-     return True
+         return validation
+     if int(value) > 0:
+         validation = True
+     return validation
 
 width = input('Enter the width of the chess board: ')
 height = input('Enter the height of the chess board: ')
