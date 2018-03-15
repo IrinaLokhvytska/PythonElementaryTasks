c1d355a173529916cd2a8ca99c970a00371c771f
IrinaLokhvytska <irinaramzaeva2@gmail.com>
2018-03-15 12:58:29 +0200

    Update numbers.py

--- ElementaryTasksSS/numbers.py
+++ ElementaryTasksSS/numbers.py
@@ -14,39 +14,58 @@ simple_number = (
     'девять'
 )
 
+
+number_clarification = {
+    2: 'тысяча',
+    3: 'миллион',
+    4: 'миллиард',
+    5: 'триллион',
+    6: 'квадриллион',
+    7: 'квинтиллион',
+    8: 'секстиллион',
+    9: 'септиллион',
+    10: 'октиллион',
+    11: 'нониллион',
+    12: 'дециллион'
+    }
+
 def complete_dictionary():
     for i in range(10):
         dict_numbers[i] = list()
         dict_numbers[i].append(simple_number[i])
-        complete_dozen(i)
-        complete_hundred(i)
-        if i > 1:
-            complete_greater_dozen(i)
+        if i == 0:
+            continue
+        else:
+            complete_dozen(i)
+            complete_hundred(i)
 
-def complete_greater_dozen(i):
-    if i in (2, 3):
-        dict_numbers[i].append(simple_number[i] + 'дцать')
-    elif i in range(5, 10):
-        dict_numbers[i].append(simple_number[i] + 'десят')
+def complete_dozen_for_one(i):
+    str = 'надцать'
+    dozens = list()
+    for y in range(10):
+        if y == 0:
+            dozens.append('десять')
+        elif y in (1, 3):
+            dozens.append(simple_number[y] + str)
+        elif y == 2:
+            dozens.append(simple_number[y][:-1] + 'у' + str)
+        else:
+            dozens.append(simple_number[y][:-1] + str)
+    dict_numbers[i].append(dozens)
+
+def complete_dozen(i):
+    if i == 1:
+        complete_dozen_for_one(i)
+    elif i in (2, 3):
+        dict_numbers[i].append(simple_number[i] +'дцать')
     elif i == 4:
         dict_numbers[i].append('сорок')
     else:
-        dict_numbers[i].append(simple_number[i][:-1] + str)
-
-def complete_dozen(i):
-    str = 'надцать'
-    if i == 0:
-        dict_numbers[i].append('десять')
-    elif i in (1, 3):
-        dict_numbers[i].append(simple_number[i] + str)
-    elif i == 2:
-        dict_numbers[i].append(simple_number[i][:-1] + 'е' + str)
-    else:
-        dict_numbers[i].append(simple_number[i][:-1] + str)
+        dict_numbers[i].append(simple_number[i] + 'десят')
 
 def complete_hundred(i):
     if i == 1:
-        dict_numbers[i].append('сто')
+        dict_numbers[i].append('sto')
     elif i == 2:
         dict_numbers[i].append(simple_number[i][:-1] + 'есте')
     elif i in (3, 4):
@@ -54,81 +73,59 @@ def complete_hundred(i):
     else:
         dict_numbers[i].append(simple_number[i] + 'сот')
 
-number_clarification = {
-    (4, 7): 'тысяча',
-    (7, 10): 'миллион',
-    (10, 13): 'миллиард',
-    (13, 16): 'триллион',
-    (16, 19): 'квадриллион',
-    (19, 22): 'квинтиллион',
-    (22, 25): 'секстиллион',
-    (25, 28): 'септиллион',
-    (28, 31): 'октиллион',
-    (31, 34): 'нониллион',
-    (34, 37): 'дециллион'
-    }
-
 def get_simple_number(n):
-    return simple_number[int(n)]
+    return dict_numbers[int(n)][0]
 
 def get_from_ten_to_twenty(n):
     first, second = map(int, n)
-    if second > 0:
-        return dict_numbers[first][1]
-    else:
-        return dict_numbers[second][1]
+    return dict_numbers[first][1][second]
 
 def get_dozen_number(n):
     if int(n) in range (10, 20):
         return get_from_ten_to_twenty(n)
     elif int(n) in range(1, 10):
         return get_simple_number(n)
+    elif int(n) == 0:
+        return ''
     else:
-        return dict_numbers[int(n[0])][3] + ' ' + dict_numbers[int(n[1])][0]
+        n = str(int(n))
+        return dict_numbers[int(n[0])][1] + ' ' + dict_numbers[int(n[1])][0]
 
 def get_hundred_number(n):
+    print(n)
     if int(n) in range (100, 1000):
         return dict_numbers[int(n[0])][2] + ' ' + get_dozen_number(n[1:])
     elif int(n) in range (10, 99):
         return get_dozen_number(n)
+    elif int(n) == 0:
+        return '000'
     else:
-        get_simple_number(n)
+        return get_simple_number(n)
 
 def split_number(n):
-    res = list()
     complete_dictionary()
+    res = list()
+    n_without_zero = str(int(n))
     convert_function = ((get_hundred_number, 3), (get_simple_number, 1), (get_dozen_number, 2))
     while n:
-        x = int(len(n)) % 3
+        x = int(len(n_without_zero)) % 3
         res.append(convert_function[x][0](n[0: convert_function[x][1]]))
         n = n[convert_function[x][1]:]
-    return res
+    return (res)
 
 def show_result(n):
-    length = len(split_number(n))
+    numbers = split_number(n)
+    length = int(len(numbers))
+    output = ''
+    for i in numbers:
+        if i == '000':
+            length -= 1
+            continue
+        else:
+            output += i
+            if length >= 2:
+                output += ' ' + number_clarification[length] + ' '
+                length -= 1
+    return output
 
-def validation(number):
-    if check_empty_value(number) and check_numbers(number):
-        split_number(number)
-    else:
-        pass
-
-def check_empty_value(value):
-    '''Check that input value isn't empty'''
-    validation = False
-    if value:
-        validation = True
-    return validation
-
-def check_numbers(value):
-     '''Check that input value can be converted to integer'''
-     validation = False
-     try:
-         int(value)
-     except ValueError:
-         return validation
-     if int(value) >= 0:
-         validation = True
-     return validation
-
-number = input('Enter the number to convert: ')
+print(show_result('10000056'))
