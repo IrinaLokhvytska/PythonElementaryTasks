f674f02afbf083b2efaad57941dec411e29fef85
IrinaLokhvytska <irinaramzaeva2@gmail.com>
2018-03-26 22:21:19 +0300

    Create url_parse.py

---
+++ url_parse.py
@@ -0,0 +1,16 @@
+import re
+import requests
+
+html = input()
+response = requests.get(html)
+if response.status_code == 200:
+  content = response.text
+  urls = re.findall(r'''<a\s.*?href=["']([\w\d-].+?)["'].?>''', content)
+domains = set()
+for url in urls:
+    domain = url.split("//")[-1].split("/")[0].split('?')[0].split(':')[0]
+    domains.add(domain)
+output = list(domains)
+output.sort()
+for el in output:
+    print(el)
